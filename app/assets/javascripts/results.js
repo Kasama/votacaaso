6ff2c3bae9fef2d5554b8bc16c7f57ca77dddb02
Kasama <ralegro@bol.com.br>
2016-06-13 23:39:15 -0300

    fixed message break

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -10,6 +10,6 @@ $(document).on('page:load ready', function(){
 	$('table').wrap('<div class="table-responsive"></div>');
 	$('.read-reason').click(function() {
 		$('#modal-head').text($(this).data('header'));
-		$('#modal-text').text(($(this).data('body')).replace(/(:?\r?\n|\r)/g, '<br />'));
+		$('#modal-text').html($(this).data('body').replace(/(:?\r?\n|\r)/g, '<br />'));
 	});
 });
